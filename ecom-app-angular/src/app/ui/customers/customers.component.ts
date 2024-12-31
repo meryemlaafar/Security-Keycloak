691f7175702f7194b611488851de3fc37365a663
Yahya ELMALKI <yahya.elmalki09@gmail.com>
2024-12-31 17:39:55 +0100

    securiser la partie frond end avec ajout de initializekeycloack et guards

--- ecom-app-angular/src/app/ui/customers/customers.component.ts
+++ ecom-app-angular/src/app/ui/customers/customers.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-customers',
-  templateUrl: './customers.component.html',
-  styleUrl: './customers.component.css'
+    selector: 'app-customers',
+    templateUrl: './customers.component.html',
+    styleUrl: './customers.component.css',
+    standalone: false
 })
 export class CustomersComponent {
 
--- ecom-app-angular/src/app/ui/products/products.component.ts
+++ ecom-app-angular/src/app/ui/products/products.component.ts
@@ -2,9 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrl: './products.component.css',
+    standalone: false
 })
 export class ProductsComponent implements OnInit{
   public products:any;
